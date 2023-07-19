52560c64142f9331fd556605b4aaca8a4e7b3a72
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-19 17:51:59 +0900

    [FE] Modify Entrance Components
    - 시작 화면 컴포넌트 기본 틀 제작

--- frontend/artlink-front/src/commponents/Entrance/EntranceLogo.tsx
+++ frontend/artlink-front/src/commponents/Entrance/EntranceLogo.tsx
@@ -1,10 +1,14 @@
 // import React from "react";
+import styles from "./EntranceLogo.module.css";
+import Art from "../../assets/Art.png";
+import Link from "../../assets/Link.png";
 
 function EntranceLogo() {
   return (
-    <>
-      <h1>EntranceLogo</h1>
-    </>
+    <div className={styles.logo}>
+      <img src={Art} alt="Art" />
+      <img src={Link} alt="Link" />
+    </div>
   );
 }
 
---
+++ frontend/artlink-front/src/commponents/Entrance/LogIn.tsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import styles from "./LogIn.module.css";
+
+function LogIn() {
+  return (
+    <div className={styles.container}>
+      <h1>LogIn</h1>
+      <input type="text" placeholder="ID" />
+      <input type="text" placeholder="Password" />
+      <a href="">아이디/비밀번호를 잊어버리셨나요?</a>
+      {/* <a href="">Did you forget ID/Password?</a> */}
+      <div className={styles["button-wrapper"]}>
+        <button>회원 가입</button>
+        {/* <button>Sign up</button> */}
+        <button>로그인</button>
+        {/* <button>Log in</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;

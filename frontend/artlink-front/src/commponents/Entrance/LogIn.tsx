cf7e06d8815773adbffa4b9124d4e73fc6e87799
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-20 20:28:10 +0900

    [FE] Modify "Entrance" Components
    - Login, SingUp 컴포넌트 수정 및 css 수정

--- frontend/artlink-front/src/commponents/Entrance/LogIn.tsx
+++ frontend/artlink-front/src/commponents/Entrance/LogIn.tsx
@@ -4,16 +4,16 @@ import styles from "./LogIn.module.css";
 function LogIn() {
   return (
     <div className={styles.container}>
-      <h1>LogIn</h1>
-      <input type="text" placeholder="ID" />
-      <input type="text" placeholder="Password" />
+      <h1>Log In</h1>
+      <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
       <a href="">아이디/비밀번호를 잊어버리셨나요?</a>
       {/* <a href="">Did you forget ID/Password?</a> */}
       <div className={styles["button-wrapper"]}>
         <button>회원 가입</button>
         {/* <button>Sign up</button> */}
-        <button>로그인</button>
-        {/* <button>Log in</button> */}
+        <button type="submit">로그인</button>
+        {/* <button type="submit">Log In</button> */}
       </div>
     </div>
   );

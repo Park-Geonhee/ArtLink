702ee6f31f493beb6bb02a40d20e1b07c77b34e8
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-20 15:13:02 +0900

    [FE] Update ExhibitionTable component
    - ExhibitionTable 컴포넌트 및 기본 스타일 제작

--- frontend/artlink-front/src/commponents/ViewExhibition/ExhibitionTable.tsx
+++ frontend/artlink-front/src/commponents/ViewExhibition/ExhibitionTable.tsx
@@ -1,8 +1,25 @@
 // import React from 'react';
+import styles from "./ExhibitionTable.module.css";
+import Mona from "../../assets/모나리자.jpg";
+import Perl from "../../assets/진주귀걸이.jpg";
+
+// 관람한 전시회 목록에 띄워질 박스
+// 전시회의 작품 목록이 썸네일로 쓰임.
+
+/*
+사진 서버에서 불러오는 코드
+*/
+
 function ExhibitionTable() {
   return (
     <>
-      <h1>ExhibitionTable</h1>
+      <div className={styles.container}>
+        <h1>ExhibitionTable</h1>
+        <div className={styles["img-wrapper"]}>
+          <img src={Mona} alt="모나리자" />
+          <img src={Perl} alt="진주귀걸이" />
+        </div>
+      </div>
     </>
   );
 }

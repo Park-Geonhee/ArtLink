b0441fb83ec171669f0b2baa8519376729b517ef
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-21 17:04:20 +0900

    [FE] Update ProfileBox component
    - ProfileBox 컴포넌트 프로토타입 제작(미완)
    - 테스트용 프로필 사진 추가

--- frontend/artlink-front/src/commponents/MyPage/ProfileBox.tsx
+++ frontend/artlink-front/src/commponents/MyPage/ProfileBox.tsx
@@ -1,11 +1,45 @@
-// import React from 'react';
+import { useState, ChangeEvent } from "react";
+import styles from "./ProfileBox.module.css";
 
-// 프로필 정보를 넣을 컴포넌트
-function ProfileBox() {
+const ProfileBox: React.FC = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result as string);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <>
-      <h1>ProfileBox</h1>
-    </>
+    <div>
+      <div>
+        {image ? (
+          <img
+            src={image}
+            alt="Profile"
+            style={{ width: "200px", height: "200px" }}
+          />
+        ) : (
+          <div>No Image</div>
+        )}
+      </div>
+      <input
+        type="file"
+        accept="image/*"
+        id="file"
+        onChange={handleImageChange}
+        style={{ display: "none" }}
+      />
+      <label htmlFor="file">파일 업로드</label>
+    </div>
   );
-}
+};
+
 export default ProfileBox;

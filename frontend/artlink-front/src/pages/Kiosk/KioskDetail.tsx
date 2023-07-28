a3db6f076b26367129e3795493f5f4443397aa36
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-28 17:04:27 +0900

    [FE] Downgrade KioskDetail
    - 작품 목록들 캐러셀처럼 넘기게 하기 위해 어제 했던거 갈아버림
    - 너무 슬픔
    - 엉엉 난 쓰레기야

--- frontend/artlink-front/src/pages/Kiosk/KioskDetail.tsx
+++ frontend/artlink-front/src/pages/Kiosk/KioskDetail.tsx
@@ -1,55 +1,45 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import MainLogo from "../../commponents/Base/MainLogo";
 import styles from "./KioskDetail.module.css";
+import BoxContainer from "./BoxContainer";
 
-// TODO: Edit 기능 추가 필요
+// 요소 삭제 시 삭제 애니메이션 넣으면 어떨까
 
 function KioskDetail() {
   // 서버로부터 가져온 작품 정보
-  const artworksInfo = [
-    { title: 1 },
-    { title: 2 },
-    { title: 3 },
-    { title: 4 },
-    { title: 5 },
-    { title: 6 },
-    { title: 7 },
-  ];
+  // const artworksInfo = [
+  //   { title: "1" },
+  //   { title: "2" },
+  //   { title: "3" },
+  //   { title: "4" },
+  //   { title: "5" },
+  //   { title: "6" },
+  //   { title: "7" },
+  //   { title: "8" },
+  //   { title: "9" },
+  //   { title: "10" },
+  // ];
 
   // 서버에 보낼 최종 데이터는 artworks다.
-  const [artworks, setArtworks] = useState(artworksInfo);
+  // const [artworks, setArtworks] = useState(artworksInfo);
 
   // Delete 버튼 클릭 시 요소 삭제
-  const handleDelete = (index: number) => {
-    const updatedArtworks = [
-      ...artworks.slice(0, index),
-      ...artworks.slice(index + 1),
-    ];
-    setArtworks(updatedArtworks);
-  };
-
-  // 요소 삭제 시 삭제 애니메이션 넣으면 어떨까
+  // const handleDelete = (index: number) => {
+  //   const updatedArtworks = [
+  //     ...artworks.slice(0, index),
+  //     ...artworks.slice(index + 1),
+  //   ];
+  //   setArtworks(updatedArtworks);
+  // };
 
   return (
     <>
       <MainLogo />
       <div className={styles["container-wrapper"]}>
-        {artworks.map((artwork, index) => (
-          <div className={styles["artwork-container"]}>
-            <div className={`${styles.artwork} ${styles.neu}`} key={index}>
-              <div>{artwork.title}</div>
-            </div>
-            <div className={styles["button-wrapper"]}>
-              <button className={`${styles.neu} ${styles.edit}`}>Edit</button>
-              <button
-                className={`${styles.neu} ${styles.delete}`}
-                onClick={() => handleDelete(index)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
+        <BoxContainer></BoxContainer>
+        <BoxContainer></BoxContainer>
+        <BoxContainer></BoxContainer>
+        <BoxContainer></BoxContainer>
       </div>
     </>
   );

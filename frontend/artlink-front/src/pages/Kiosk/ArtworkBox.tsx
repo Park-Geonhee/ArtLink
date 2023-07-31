0d1e16d38bf877687fa6c88b5fcdb8cd8193834f
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-07-31 14:15:47 +0900

    [FE] Update KioskDetail Page
    - 키오스크의 작품 열람 페이지 기초 기능 및 요소 구현

--- frontend/artlink-front/src/pages/Kiosk/ArtworkBox.tsx
+++ frontend/artlink-front/src/pages/Kiosk/ArtworkBox.tsx
@@ -2,27 +2,31 @@ import styles from "./ArtworkBox.module.css";
 
 // 작품의 정보를 띄울 박스, delete로 작품 삭제 가능
 
-interface Props {
+interface Artwork {
+  id: number;
   title: string;
-  // onClickDelete: () => void;
 }
 
-// function ArtworkBox({ title, onClickDelete }: Props) {
-function ArtworkBox({ title }: Props) {
+interface Props {
+  artwork: Artwork;
+  onClickDelete: (id: number) => void;
+}
+
+function ArtworkBox({ artwork, onClickDelete }: Props) {
   return (
     <div className={styles["artwork-box"]}>
       <div className={`${styles.artwork} ${styles.neu}`}>
         {/*작품의 정보가 들어갈 곳*/}
-        <div>{title}</div>
+        <div>{artwork.title}</div>
       </div>
-      {/* <div className={styles["button-wrapper"]}>
+      <div className={styles["button-wrapper"]}>
         <button
           className={`${styles.neu} ${styles.delete}`}
-          onClick={onClickDelete}
+          onClick={() => onClickDelete(artwork.id)}
         >
           Delete
         </button>
-      </div> */}
+      </div>
     </div>
   );
 }

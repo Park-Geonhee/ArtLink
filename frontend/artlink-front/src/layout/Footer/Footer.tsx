39be7e0349a414b1a3783b59a331fe5f217b350f
조준하 <whwnsgk830@naver.com>
2023-08-01 13:52:33 +0900

    [FE] Update Login, Signup, Footer components
    <Login, Signup>
    - 폰트, 인풋필드 변경
    - Enter키로 Submit가능하게 변경
    
    <Footer >
    -Footer 초안 도입

--- frontend/artlink-front/src/layout/Footer/Footer.tsx
+++ frontend/artlink-front/src/layout/Footer/Footer.tsx
@@ -1,19 +1,54 @@
 // import React from 'react';
 import { useLocation } from "react-router-dom";
-import footercss from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import "./Footer.css";
 
 function Footer() {
   const location = useLocation();
-  const isKiosk = location.pathname.includes("kiosk"); 
+  const isKiosk = location.pathname.includes("kiosk");
   const is3D = location.pathname.includes("3d");
-  
+
   return (
     <>
-    {!isKiosk&& !is3D && (
-      <div className={footercss.footer}>
-        <h1>Footer</h1>
-      </div>
-    )}
+      {!isKiosk && !is3D && (
+        <div className="footBody">
+          <section className="footer">
+            <div className="social">
+              <a href="#">
+                <FontAwesomeIcon icon={faCamera} size="2x" />
+              </a>
+              <a href="#">
+                <i className="fab fa-snapchat"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+            </div>
+            <ul className="list">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Terms</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+            <p className="copyright">SSAFY PJT1 - Accpted 202 @ 2023</p>
+          </section>
+        </div>
+      )}
     </>
   );
 }

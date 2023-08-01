adc795388a64035bc32382f92eedef8a94e4028c
ppsrac <ppsracchriskim@gmail.com>
2023-08-01 21:07:26 +0900

    [BE] modify: modified mqtt.apps.py, add TODO in mqtt.apps.py

--- backend/BS/mqtt/apps.py
+++ backend/BS/mqtt/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.conf import settings
 import paho.mqtt.client as mqtt
 import threading
-import requests
+import requests, json
 
 class MqttConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -11,11 +12,18 @@ class MqttConfig(AppConfig):
     MQTT_BROKER_PORT = 1883
     MQTT_TOPIC = "my_mqtt_topic"
 
-    request_url = ""
+
+    # TODO: on_message 함수 수정.
+    request_url = getattr(settings, 'DJANGO_REQUEST_PATH', 'none') #이것이 에러날 수도 있음 (장고 로딩 순서 이슈) 에러날 경우 밑의 url사용.
+    #request_url = 'http://localhost:8080/device/click/'
 
     def on_message(self, client, userdata, message):
         payload = message.payload.decode()
-        response = requests.post(self.OTHER_SERVER_URL, data={"message": payload})
+        # TODO: data에 넣을 것 수정. 아마 json.loads를 사용하는 것이 나을듯. 아니라면 오류 뜰 수도 있음.
+        data = {}
+        # TODO: payload를 이용하여 data 구성.
+        data = json.dumps(data)
+        response = requests.post(self.request_url, data)
         print("Response from other server:", response.text)
 
     def on_connect(self, client, userdata, flags, rc):

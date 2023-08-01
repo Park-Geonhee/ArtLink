b786cea57c651c3ecb6c2f5dd2c16f37ee5a5e70
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-01 11:05:45 +0900

    [FE] Resolve conflict issue
    - Modal.tsx 충돌 문제 해결

--- frontend/artlink-front/src/commponents/Base/Form/SignupModal/Modal.tsx
+++ frontend/artlink-front/src/commponents/Base/Form/SignupModal/Modal.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-=======
 import { useState, useEffect } from "react";
->>>>>>> 495435712c3197879ee38ad7eb859adc50d06c11
 import ModalBase from "./ModalBase";
 import CardModal from "./CardModal";
 import { useNavigate } from "react-router-dom";

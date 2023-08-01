495435712c3197879ee38ad7eb859adc50d06c11
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-01 10:57:32 +0900

    [FE] Update KioskExit
    - 키오스크 종료 화면 제작

--- frontend/artlink-front/src/pages/Kiosk/KioskExit.tsx
+++ frontend/artlink-front/src/pages/Kiosk/KioskExit.tsx
@@ -1,8 +1,35 @@
-// import React from 'react';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./KioskExit.module.css";
+import KioskMainLogo from "./KioskMainLogo";
+
 function KioskExit() {
+  const [countdown, setCountdown] = useState<number>(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate("/kiosk/home");
+    }
+  }, [countdown, navigate]);
+
   return (
     <>
-      <h1>KioskExit</h1>
+      <KioskMainLogo />
+      <div className={styles["notice-container"]}>
+        <div className={styles["notice-box"]}>
+          <div className={styles["notice-text"]}>{"Good Bye"}</div>
+        </div>
+      </div>
+      <h1>{countdown}초 후 홈으로 돌아갑니다.</h1>
     </>
   );
 }

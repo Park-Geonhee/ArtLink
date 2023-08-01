cc892181050087d0172e848ce7f56aeef43cc4cb
김진현 <adgzxc12@naver.com>
2023-08-01 17:49:01 +0900

    feat : gallery, exhibition api

--- backend/WAS/projecttest1/src/main/java/com/example/projecttest1/service/ExhibitionService.java
+++ backend/WAS/projecttest1/src/main/java/com/example/projecttest1/service/ExhibitionService.java
@@ -1,26 +1,44 @@
 package com.example.projecttest1.service;
 
 import com.example.projecttest1.dto.ExhibitionRequestDto;
+import com.example.projecttest1.dto.ExhibitionResponseDto;
 import com.example.projecttest1.entity.Exhibition;
 import com.example.projecttest1.entity.Gallery;
+import com.example.projecttest1.exception.exhibition.ExhibitionNotFoundException;
 import com.example.projecttest1.repository.ExhibitionRepository;
+import com.example.projecttest1.repository.GalleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class ExhibitionService {
 
     @Autowired
     private ExhibitionRepository exhibitionRepository;
-    public void registerExhibition(ExhibitionRequestDto requestDto, Gallery gallery) {
+
+    @Autowired
+    private GalleryRepository galleryRepository;
+
+    public Exhibition registerExhibition(ExhibitionRequestDto requestDto, String galleryName) {
         // 검증
+        Gallery gallery = galleryRepository.findByUsername(galleryName);
         Exhibition exhibition = new Exhibition();
         exhibition.setExhibitionName(requestDto.getExhibitionName());
         exhibition.setGallery(gallery);
         exhibition.setExhibitionExplanation("");
-        exhibition.setCreatedAt(LocalDateTime.now());
-        exhibitionRepository.save(exhibition);
+        exhibition.setCreatedAt(LocalDate.now());
+        return exhibitionRepository.save(exhibition);
+    }
+
+    public Exhibition findById(Integer id) {
+        return exhibitionRepository.findById(id).orElseThrow(()->new ExhibitionNotFoundException());
+    }
+
+    public List<Exhibition> selectAllExhibitions(String galleryName) {
+        Gallery gallery = galleryRepository.findByUsername(galleryName);
+        return exhibitionRepository.findAllByGallery(gallery);
     }
 }

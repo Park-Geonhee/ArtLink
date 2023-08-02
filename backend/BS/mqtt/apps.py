44af05fdd3bd492bebe28fde0972a7179448ebc6
박건희 <geon4415@naver.com>
2023-08-02 14:06:11 +0900

    feature : add MQTT data preprocessing logic

--- backend/BS/mqtt/apps.py
+++ backend/BS/mqtt/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.conf import settings
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import threading
 import requests, json
 
@@ -8,26 +9,68 @@ class MqttConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mqtt'
 
-    MQTT_BROKER_HOST = "mqtt.example.com"
+    MQTT_BROKER_HOST = "localhost"
     MQTT_BROKER_PORT = 1883
-    MQTT_TOPIC = "my_mqtt_topic"
+    MQTT_TOPIC = "test/#"
 
-
-    # TODO: on_message 함수 수정.
     request_url = getattr(settings, 'DJANGO_REQUEST_PATH', 'none') #이것이 에러날 수도 있음 (장고 로딩 순서 이슈) 에러날 경우 밑의 url사용.
-    #request_url = 'http://localhost:8080/test/'
+    #request_url = 'http://localhost:8000/test/'
 
     # 테스트용 api 확인
     # 반드시 data를 data = { "num" : 3 } 형태의 json 파일로 보낼것.
     def on_message(self, client, userdata, message):
+        # payload는 json 형태로 수신
         payload = message.payload.decode()
-        # TODO: data에 넣을 것 수정. 아마 json.loads를 사용하는 것이 나을듯. 아니라면 오류 뜰 수도 있음.
-        data = {}
-        # TODO: payload를 이용하여 data 구성.
-        data = json.dumps(data)
-        response = requests.post(self.request_url, data)
-        print("Response from other server:", response.text)
+        data = json.loads(payload)
+        # print("your data : ", data)
+        
+        event = data["E"]
+        # 등록 요청인 경우
+        if event == "R":
+            # data preprocessing 
+            tag_id = data["T"]
+            link_cnt = data["LC"]
+            if link_cnt < 3 :
+                res = 2
+                publish.single(tag_id, res,hostname = self.MQTT_BROKER_HOST, port = self.MQTT_BROKER_PORT)
+                return
+            # make request body
+            req_body = {}
+            req_body['deviceid'] = tag_id
+            for i in range(3):
+                now_anchor = "anchor" + str(i+1)
+                now_range = "d" + str(i+1)
+                req_body[now_anchor] = int(data["L"][i][0])
+                req_body[now_range] = float(data["L"][i][1])
+            # 정제된 데이터는 다시 json으로 변환하여 서버에 요청
+            req_body = json.dumps(req_body)
+            response = requests.post(self.request_url, data)
+            print("Response from other server:", response.text)
+            # 요청 성공시, response.status_code = 200 (int) 로 넘어온다.
+            # IoT 기기에게 잘 등록됬으면 mqtt publish 하여 완료 신호 송신
+            returnData = None
+            if response.status_code == 200:
+                returnData = 1
+                # publish.single 의 payload는 데이터 형식이 json 이어야 한다?
+            else:
+                returnData = 2    
+            publish.single( tag_id, returnData, hostname=self.MQTT_BROKER_HOST, port=self.MQTT_BROKER_PORT)
 
+        '''
+        # 삭제 요청인 경우
+        if event == "D":
+            # deviceid 는 tag_id와 같습니다.
+            # deviceId = data["T"]
+            # 삭제 요청 수행
+            response = requests.delete(self.request_url, data)
+            # 요청 response 획득
+            returnData = None
+            if response.status_code == 200:
+                returndData = 0
+            else:
+                returnData = 2
+            publish.single(tag_id, returnData, hostname=self.MQTT_BROKER_HOST, port=self.MQTT_BROKER_PORT)
+        '''
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
         client.subscribe(self.MQTT_TOPIC)

46c4af690e6b0a61b53b80ded5c236ac7fafffa7
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-02 13:56:50 +0900

    [FE] Add IoTBoard
    - 기기 관리 페이지인 IoTBoard 추가

---
+++ frontend/artlink-front/src/commponents/Iot/IotBoard.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import styles from "./IoTBoard.module.css";
+
+// import React from 'react';
+
+function IoTBoard() {
+  return <></>;
+}
+export default IoTBoard;

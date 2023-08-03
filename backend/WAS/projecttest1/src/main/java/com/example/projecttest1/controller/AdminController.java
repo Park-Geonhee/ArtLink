12c739ce275cf571004f7a01f77fced452e3f2fe
김진현 <adgzxc12@naver.com>
2023-08-03 15:07:13 +0900

    feat : admin retrieve user detail

--- backend/WAS/projecttest1/src/main/java/com/example/projecttest1/controller/AdminController.java
+++ backend/WAS/projecttest1/src/main/java/com/example/projecttest1/controller/AdminController.java
@@ -1,15 +1,17 @@
 package com.example.projecttest1.controller;
 
+import com.example.projecttest1.dto.GalleryManagementDto;
 import com.example.projecttest1.dto.GalleryResponseDto;
+import com.example.projecttest1.dto.UserDetailResponseDto;
 import com.example.projecttest1.dto.UserResponseDto;
 import com.example.projecttest1.entity.Gallery;
+import com.example.projecttest1.entity.User;
 import com.example.projecttest1.service.GalleryService;
 import com.example.projecttest1.service.UserService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.ResponseExtractor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,24 +35,33 @@ public class AdminController {
         return ResponseEntity.ok(Map.of("users", usersDto));
     }
 
+    @GetMapping("/users/{id}")
+    private ResponseEntity<UserDetailResponseDto> findByIdUser(@PathVariable Integer id) {
+        User user = userService.findById(id);
+        UserDetailResponseDto responseDto = new UserDetailResponseDto(
+                user.getUsername(), user.getPhoneNumber(), user.getNickname());
+        return ResponseEntity.ok(responseDto);
+    }
+
     @GetMapping("/galleries")
-    public ResponseEntity<Map<String, List<GalleryResponseDto>>> findAllGalleries() {
-        List<GalleryResponseDto> galleriesDto = new ArrayList<>();
-        galleryService.findAll().forEach( g -> galleriesDto.add(new GalleryResponseDto(g.getUsername(), g.getGalleryName(), g.getAccepted(), g.getDescription())));
+    public ResponseEntity<Map<String, List<GalleryManagementDto>>> findAllGalleries() {
+        List<GalleryManagementDto> galleriesDto = new ArrayList<>();
+        galleryService.findAll().forEach( g -> galleriesDto.add(new GalleryManagementDto(g.getId(), g.getUsername(), g.getGalleryName(), g.getAccepted(), g.getDescription())));
         return ResponseEntity.ok(Map.of("galleries", galleriesDto));
     }
 
     @GetMapping("/galleries/{id}")
-    public ResponseEntity<GalleryResponseDto> findByIdGallery(@PathVariable Integer id) {
+    public ResponseEntity<GalleryManagementDto> findByIdGallery(@PathVariable Integer id) {
         Gallery gallery = galleryService.findById(id);
-        GalleryResponseDto responseDto = new GalleryResponseDto(gallery.getUsername(), gallery.getGalleryName(), gallery.getAccepted(), gallery.getDescription());
+        GalleryManagementDto responseDto = new GalleryManagementDto(gallery.getId(), gallery.getUsername(), gallery.getGalleryName(), gallery.getAccepted(), gallery.getDescription());
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/galleries/{id}/accept")
-    public ResponseEntity<GalleryResponseDto> acceptGallery() {
-//        galleryService.
-        return null;
+    public ResponseEntity<GalleryManagementDto> acceptGallery(@PathVariable Integer id) {
+        Gallery gallery = galleryService.acceptGallery(id);
+        GalleryManagementDto responseDto = new GalleryManagementDto(gallery.getId(), gallery.getUsername(), gallery.getGalleryName(), gallery.getAccepted(), gallery.getDescription());
+        return ResponseEntity.ok(responseDto);
     }
 
 

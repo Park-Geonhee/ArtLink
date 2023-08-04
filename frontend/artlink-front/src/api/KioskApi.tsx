c7258d008f3c3eca5e6d0734d6fbf1be9e7abcec
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-04 17:19:16 +0900

    [FE] Modify KioskApi.tsx
    - TODO list 추가
    - api 추가

--- frontend/artlink-front/src/api/KioskApi.tsx
+++ frontend/artlink-front/src/api/KioskApi.tsx
@@ -1,8 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 
 /*
-방문객 작품 삭제(DELETE): selections/devices/{deviceId}
+사후 데이터 삭제(DELETE): selections/devices/{deviceId}
 키를 통한 사후 데이터 조회(GET): /postevents/{Key}
+*/
+
+/*
+태깅
+-> RFID 기기에서 /devices/{deviceId}(DELETE) api로 userKey 받아옴
+-> 기기에서 받아온 userKey를 키오스크 홈의 input에 넣기
+-> 입력 후 제출 버튼 누를 시 해당 키가 다음 페이지로 넘겨지며 해당 페이지로 넘어감
 */
 
 const defaultBackendUrl = import.meta.env.VITE_APP_BACKEND_URL;
@@ -15,20 +22,23 @@ export interface Data {
   [key: string]: string | number;
 }
 
-// 방문객 작품 삭제(DELETE): selections/devices/{deviceId}
-export const deleteDevice = async (deviceId: number): Promise<Data> => {
+// 사후 데이터 삭제(DELETE): /postevents/{userKey}/drawings/{deviceId}
+export const deleteArtwork = async (
+  userKey: string,
+  drawingId: number
+): Promise<Data> => {
   try {
     const response: AxiosResponse<Data> = await axios.delete(
-      createUrl(`selections/devices/${deviceId}`)
+      createUrl(`/postevents/${userKey}/drawings/${drawingId}`)
     );
     return response.data;
   } catch (error) {
-    console.error("방문객 작품 삭제에 실패했습니다.", error);
+    console.error("사후 데이터 삭제에 실패했습니다.", error);
     throw error;
   }
 };
 
-// 키를 통한 사후 데이터 조회(GET): /postevents/{Key}
+// 키를 통한 사후 데이터 조회(GET): /postevents/{userKey}
 export const getPostevents = async (userKey: string): Promise<Data[]> => {
   try {
     const accessToken = localStorage.getItem("access_token");

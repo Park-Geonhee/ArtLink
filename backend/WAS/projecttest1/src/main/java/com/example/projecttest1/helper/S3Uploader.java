6ef83032927071f19d1312146dfb6c2f6645fe49
ppsrac <ppsracchriskim@gmail.com>
2023-08-04 09:01:49 +0900

    Revert "[BE] feat: connect with djangoServer"
    This reverts commit 007f2e6483f56304e04a5220b1eb876787e390b6.

--- backend/WAS/projecttest1/src/main/java/com/example/projecttest1/helper/S3Uploader.java
+++ backend/WAS/projecttest1/src/main/java/com/example/projecttest1/helper/S3Uploader.java
@@ -56,12 +56,12 @@ public class S3Uploader {
     }
 
     //이미 올라갔던 사진 삭제 후 재 생성.
-    public String modify(String folderName, String oldFileName, String newFileName, MultipartFile multipartFile) throws Exception{
+    public String modify(String folderName, String fileName, MultipartFile multipartFile) throws Exception{
         try{
-            if(!delete(oldFileName)){
+            if(!delete(fileName)){
                 throw new S3DeleteFailException("Delete failed");
             }
-            return upload(folderName, newFileName, multipartFile);
+            return upload(folderName, fileName, multipartFile);
         }
         catch(S3DeleteFailException de){
             de.printStackTrace();

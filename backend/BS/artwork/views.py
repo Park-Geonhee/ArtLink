da9620f23b3cee255ea561d237b50a5fd5f3ba67
ppsrac <ppsracchriskim@gmail.com>
2023-08-07 08:33:13 +0900

    [BE] modify: modify some apis

--- backend/BS/artwork/views.py
+++ backend/BS/artwork/views.py
@@ -11,8 +11,10 @@ from django.db.models import Q
 
 from artwork.models import Artwork, Voronoipoint, Voronoiresult
 from artwork.serializers import ArtworkSerializer
-from artwork.tools import getVoronoi
+
 from exhibition.models import Exhibition
+import exhibition.services as ExhibitionServices
+import artwork.services as ArtworkServices
 
 
 @method_decorator(csrf_exempt, name = 'dispatch')
@@ -20,109 +22,77 @@ class ArtworkView(View):
     def post(self, request):
         # 미술품의 좌표, 번호, 전시회 id만 받아 DB 추가
         data = json.loads(request.body)
-        serializer = ArtworkSerializer(data = data)
         try:
             #미술품 저장.
-            serializer.create(validated_data = data)
+            Artwork.objects.create(artworkid = data['artworkid'],
+                                   exhibition = ExhibitionServices.find_by_Id(data['exhibitionid']),
+                                   coorx = data['coorx'],
+                                   coory = data['coory'])
 
             #Voronoi diagram을 뽑는 과정
-            exhibitionid = data['exhibitionid']
-            exhibition = Exhibition.objects.get(exhibitionid=exhibitionid)
+            exhibition = ExhibitionServices.find_by_Id(data['exhibitionid'])
             artworks = Artwork.objects.filter(exhibition = exhibition)
 
-            txt = str(len(artworks))
-            txt += '\n'
-            for work in artworks:
-                txt += (str(work.coorx) + ' ' + str(work.coory) + '\n')
-
             #Voronoi diagram 돌리기 요청.
-            if getVoronoi(txt, exhibition):
-                return JsonResponse({'msg':'ok'}, status=200)
+            if ArtworkServices.getVoronoi(exhibition):
+                return JsonResponse({'msg':'ok'}, status = 201)
         except Exception as e:
             print(e)
-            return JsonResponse({'msg': 'fail'}, status=404)
+            Artwork.objects.all().delete()
+            return JsonResponse({'msg': 'fail'}, status = 404)
 
     def get(self, request):
         #모든 Artwork 조회
         content = ArtworkSerializer(Artwork.objects.all(), many = True)
-        print(content)
-        return HttpResponse(status=200, content = json.dumps(content.data))
+        return JsonResponse(json.dumps(content.data), status = 200)
 
     def put(self, request): # 미술품 좌표 수정.
         data = json.loads(request.body)
 
-        artworkid = data['artworkid']
-        artwork = Artwork.objects.filter(artworkid=artworkid).first()
+        artwork = ArtworkServices.find_artwork_by_Id(data['artworkid'])
+        ArtworkServices.put_artwork(artwork, coorx = data['coorx'], coory = data['coory'])
+        artwork.save()
 
-        coorx, coory = data['coorx'], data['coory']
-        artwork.coorx, artwork.coory = coorx, coory
-
-        serializer = ArtworkSerializer()
         try:
-            # 미술품 수정
-            serializer.update(validated_data = data)
-
             # Voronoi diagram을 뽑는 과정
-            exhibitionid = data['exhibitionid']
-            exhibition = Exhibition.objects.get(exhibitionid=exhibitionid)
-            artworks = Artwork.objects.filter(exhibition=exhibition)
-
-            txt = str(len(artworks))
-            txt += '\n'
-            for work in artworks:
-                txt += (str(work.coorx) + ' ' + str(work.coory) + '\n')
+            exhibition = ExhibitionServices.find_by_Id(data['exhibitionid'])
+            artworks = ArtworkServices.find_by_exhibition(exhibition)
 
             # Voronoi diagram 돌리기 요청.
-            if getVoronoi(txt, exhibition):
-                return JsonResponse({'msg': 'ok'}, status=200)
+            if ExhibitionServices.getVoronoi(exhibition):
+                return JsonResponse({'msg': 'ok'}, status = 200)
         except Exception as e:
             print(e)
-            return JsonResponse({'msg': 'fail'}, status=404)
+            return JsonResponse({'msg': 'fail'}, status = 404)
 
-# TODO: 전시회별 수정 요청.
-# TODO: 미술품 삭제요청도 만들어야 함!!
-
-@method_decorator(csrf_exempt, name = 'dispatch')
+@method_decorator(csrf_exempt, name = "dispatch")
 class ArtworkDetailView(View):
     def delete(self, request, artworkid):
-        artwork = Artwork.objects.filter(artworkid = artworkid).first()
-
-        if artwork.exists():
-            artwork.delete()
-        else:
-            raise Exception('Artwork does not exist')
-
-        exhibition = artwork.exhibition
-        artworks = Artwork.objects.filter(exhibition = exhibition)
-        count = artworks.count()
-
-
-        serializer = ArtworkSerializer()
+        data = json.loads(request.body)
+        ArtworkServices.delete_artwork_by_Id(artworkid)
         try:
-            # 미술품 수정
-
-            # Voronoi diagram을 뽑는 과정
-
-            txt = str(len(artworks))
-            txt += '\n'
-            for work in artworks:
-                txt += (str(work.coorx) + ' ' + str(work.coory) + '\n')
+            exhibition = ExhibitionServices.find_by_Id(data['exhibitionid'])
+            artworks = ArtworkServices.find_by_exhibition(exhibition)
 
             # Voronoi diagram 돌리기 요청.
-            if getVoronoi(txt, exhibition):
+            if ExhibitionServices.getVoronoi(exhibition):
                 return JsonResponse({'msg': 'ok'}, status=200)
         except Exception as e:
             print(e)
             return JsonResponse({'msg': 'fail'}, status=404)
-
-
-
-
 
 @method_decorator(csrf_exempt, name = 'dispatch')
 class getNearbyArtwork(View):
     def get(self, request, artworkid):
         try:
+            artwork = ArtworkServices.find_artwork_by_Id(artworkid)
+            exhibition = artwork.exhibition
+            count = ArtworkServices.get_artwork_count(exhibition)
+            if count <= 1:
+                return JsonResponse({'nearby': []}, status = 200)
+            elif count == 2:
+                other_artwork = ArtworkServices.get_other_artwork(artworkid)
+                return JsonResponse({'nearby':[other_artwork.artworkid]}, status=200)
             result = Voronoiresult.objects.filter(Q(cwartworkid = artworkid) | Q(ccwartworkid = artworkid))
             nearby = []
             for line in result:

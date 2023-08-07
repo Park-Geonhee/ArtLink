cb0241568e419f45ebd3aafa71dab5da93918fcb
조준하 <whwnsgk830@naver.com>
2023-08-08 00:11:28 +0900

    [FE] Update WorksBoard, WorksCreate Pages
    <WorksBoard>
    - 로드 데이터 테이블 반영
    
    <WorksCreate>
    - 반응형 레이아웃 설계
    - 페이지 구조 재설계
    - 생성 API 연결

--- frontend/artlink-front/src/pages/Gallery/WorksBoard.tsx
+++ frontend/artlink-front/src/pages/Gallery/WorksBoard.tsx
@@ -5,39 +5,22 @@ import BackBtn from "../../commponents/Base/BackBtn";
 import { AllWorks, AllWorksRes } from "../../api/GalleryApi";
 import { useEffect, useState } from "react";
 
-interface ArtworkData {
-  [key: string]: string | number; // 키-값 데이터 타입은 이렇게만 설정해두면 됨
-}
-
 function WorksBoard() {
-  const [works, setWorks] = useState<AllWorksRes>();
+  const [works, setWorks] = useState([{}]);
   useEffect(() => {
     const fetchAllworks = async () => {
-      const allWorks = await AllWorks();
-      console.log(allWorks);
-      setWorks(allWorks);
+      const allWorks:AllWorksRes = await AllWorks();
+      console.log('전체 작품 목록', allWorks);
+      setWorks(allWorks.DrawingList);
     };
 
     void fetchAllworks();
   }, []);
-  const artworkData: ArtworkData[] = [
-    { id: 1, title: "제목1", location: "위치1", description: "설명1" },
-    { id: 2, title: "제목2", location: "위치2", description: "설명2" },
-    { id: 3, title: "제목3", location: "위치3", description: "설명3" },
-    { id: 4, title: "제목4", location: "위치3", description: "설명3" },
-    { id: 5, title: "제목5", location: "위치3", description: "설명3" },
-    { id: 6, title: "제목6", location: "위치3", description: "설명3" },
-    { id: 7, title: "제목7", location: "위치3", description: "설명3" },
-    { id: 8, title: "제목8", location: "위치3", description: "설명3" },
-    { id: 9, title: "제목8", location: "위치3", description: "설명3" },
-    { id: 10, title: "제목8", location: "위치3", description: "설명3" },
-    { id: 11, title: "제목8", location: "위치3", description: "설명3" },
-    { id: 12, title: "제목8", location: "위치3", description: "설명3" },
-  ];
 
-  const keys = Object.keys(artworkData[0]);
-  const widths = ["8%", "54%", "30%", "8%"];
-  const keyToExclude = ["description"];
+  // const keys = Object.keys(works[0]);
+  const keys = ["아티스트", "설명", "제목"];
+  const widths = ["20%", "40%", "30%"];
+  const keyToExclude = ["drawingPath","locationX","locationY"];
 
   return (
     <>
@@ -57,7 +40,7 @@ function WorksBoard() {
               className={styles.searchinput}
             />
             <button className={styles.searchbutton}>search</button>
-            <Link to="works/create">
+            <Link to="create">
               <button className={styles.addbutton}>+</button>
             </Link>
             <Link to="update">
@@ -68,7 +51,7 @@ function WorksBoard() {
         {/* 테이블 내용 */}
         <div className={styles["component-wrapper"]}>
           <InfoTable
-            data={artworkData}
+            data={works}
             pageSize={10}
             dataKeys={keys}
             columnWidths={widths}
--- frontend/artlink-front/src/pages/Gallery/WorksCreate.tsx
+++ frontend/artlink-front/src/pages/Gallery/WorksCreate.tsx
@@ -1,10 +1,99 @@
-// import React from 'react';
-import ArtworkProfileBox from "../../commponents/Info/ArtworkProfileBox.tsx";
-import ArtworkInputInfoTable from "../../commponents/Info/ArtworkInputInfoTable.tsx";
+import { useState, ChangeEvent, useRef } from "react";
 import "./Detail.css";
+import EmptyProfile from "../../assets/EmptyProfile2.svg";
 import BackBtn from "../../commponents/Base/BackBtn.tsx";
+import TextBtn from "../../commponents/Base/TextBtn.tsx";
+import { Drawing, WorkCreate } from "../../api/GalleryApi.tsx";
 
 function WorksCreate() {
+  // 전송할 폼데이터
+  const formDataRef = useRef<FormData>(new FormData());
+  const [formData, setFormData] = useState<Drawing>({
+    Name: "",
+    DrawingPath: "",
+    Description: "",
+    Artist: "",
+    LocationX: 0,
+    LocationY: 0,
+  });
+  // 각 필드와 필드에 대한 이름을 매핑한 객체
+  const fieldNames: Record<keyof Drawing, string> = {
+    Name: "제목",
+    Description: "설명",
+    Artist: "작가",
+    LocationX: "위치 X",
+    LocationY: "위치 Y",
+    DrawingPath: ""
+  };
+  const [image, setImage] = useState<string | null>(null); // 이미지 관련
+  // 생성 요청시
+  const handleAdd = () => {
+    console.log("creating");
+    for (const key in formData) {
+      if (key != "DrawingPath"){
+        formDataRef.current.append(key, formData[key]);
+      }
+    }
+    if (formDataRef.current.has("ImageFile")) {
+      void callCreateWork();
+    }
+  };
+  // 생성 API
+  const callCreateWork = async () => {
+    try {
+      const response = await WorkCreate(formDataRef.current);
+      console.log("Work created:", response);
+    } catch (error) {
+      console.error("Error creating exhibition:", error);
+    }
+  };
+  // 이미지 변경시
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result as string);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+      // 폼데이터에 파일 저장
+      formDataRef.current.append("ImageFile", file);
+    }
+  };
+  // 인풋값 변경시
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  // 인풋 필드 자동 생성
+  const renderFields = () => {
+    return Object.keys(formData).map((field, index) => {
+      if (field !== "DrawingPath") {
+        // 이미지 필드는 제외
+        return (
+          <div key={index} className="input-field">
+            <label htmlFor={field}>{fieldNames[field as keyof Drawing]} : </label>
+            <input
+              type="text"
+              id={field}
+              name={field}
+              placeholder={`${fieldNames[field as keyof Drawing]}을 입력하세요`}
+              onChange={handleInputChange}
+            />
+          </div>
+        );
+      }
+      return null;
+    });
+  };
+
   return (
     <>
       {/* 뒤로가기 & 페이지 설명 */}
@@ -14,12 +103,34 @@ function WorksCreate() {
       </div>
       {/* 작품 생성 바디 */}
       <div className="detail-container">
-        <ArtworkProfileBox />
-        <ArtworkInputInfoTable></ArtworkInputInfoTable>
+        {/* 이미지 */}
+        <div className="image-box">
+          <div style={{ width: "200px" }}>
+            {image ? (
+              <img
+                src={image}
+                alt="Profile"
+                style={{ width: "200px", height: "200px" }}
+              />
+            ) : (
+              <img src={EmptyProfile} alt="빈 프로필" />
+            )}
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            id="file"
+            onChange={handleImageChange}
+            style={{ display: "none" }}
+          />
+          <label htmlFor="file">파일 업로드</label>
+        </div>
+        {/* 텍스트 */}
+        <div className="txt-box">{renderFields()}</div>
       </div>
-      {/* 작품 정보 업데이트 버튼 */}
-      <div>
-        <button className="detail-updateBtn">CREATE</button>
+      {/* 작품 생성 버튼 */}
+      <div onClick={handleAdd}>
+        <TextBtn inner="CREATE" wid={200} hei={50} />
       </div>
     </>
   );

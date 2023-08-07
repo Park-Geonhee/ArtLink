8d94c5f128c774398dc11ef65e37c860b3147d30
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-07 16:57:58 +0900

    [FE] Modify Apitest
    - Apitest에 토큰 확인용 기능 추가

--- frontend/artlink-front/src/api/Apitest.tsx
+++ frontend/artlink-front/src/api/Apitest.tsx
@@ -1,47 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { fetchSample, Sample } from "./TestApi";
 // 해당 컴포넌트는 api통신 테스트입니다. get요청이고 post요청일시 datatosend변수로 매개변수를 전달하면 됩니다.
-import { Data, getDeviceList, registerDevice } from "./IoTApi";
 
 const Apitest: React.FC = () => {
-  const [deviceData, setDeviceData] = useState<Data[]>([]);
+  // 토큰 확인용
+  // 아래 영역 절대 지우지 말것
+  const [accessToken, setAccessToken] = useState<string | null>("");
+  const [refreshToken, setRefreshToken] = useState<string | null>("");
 
-  const regist = async () => {
+  const getData = () => {
     try {
-      const dataToSend: Data = { phoneNumber: 8201012345678, exhibitionId: 2 };
-      const data = await registerDevice(dataToSend, 1);
-      console.log(data);
+      const access = localStorage.getItem("access_token");
+      const refresh = localStorage.getItem("refresh_token");
+      setAccessToken(access);
+      setRefreshToken(refresh);
     } catch (error) {
-      console.error("기기 등록에 실패했습니다.:", error);
+      console.error("토큰값을 가져오는데 실패했습니다.", error);
     }
   };
 
-  const getData = async () => {
-    try {
-      const data = await getDeviceList();
-      console.log(data);
-      setDeviceData(data);
-    } catch (error) {
-      console.error("기기 데이터를 가져오는 데 실패했습니다: ", error);
-    }
-  };
   useEffect(() => {
-    void regist();
-    void getData(); // 데이터를 가져오는 함수를 호출합니다.
+    void getData();
   }, []);
+  // 위 영역 절대 지우지 말것
 
   return (
     <>
       <h1>Apitest</h1>
       <hr />
       <div>
-        {/* 가져온 데이터를 화면에 렌더링 */}
-        {deviceData.map((item, index) => (
-          <div key={index}>
-            <p>deviceId: {item.deviceId}</p>
-            <p>phoneNumber: {item.phoneNumber}</p>
-          </div>
-        ))}
+        {/* 아래 두 줄 절대 지우지 말것 */}
+        <p>accessToken: {accessToken}</p>
+        <p>refreshToken: {refreshToken}</p>
       </div>
     </>
   );

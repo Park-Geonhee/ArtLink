109ebd5552afcba9aebbd862a91f4ecce6af2d2c
조준하 <whwnsgk830@naver.com>
2023-08-07 11:18:57 +0900

    [FE] Update UserManager, GalleryManager, Mypage Components
    <UserManager>
    - 유저정보 디테일 디자인
    - 유저정보 디테일 API 구현
    - 유저정보 디테일 관련 필드 수정
    
    <GalleryManager>
    - 갤러리정보 디테일 디자인
    - 갤러리정보 디테일 API 구현
    - 갤러리정보 디테일 관련 필드 수정
    
    <Mypage>
    - 갤러리 이미지 삭제
    - 갤러리 디자인 변경
    - 갤러리 css 추가

--- frontend/artlink-front/src/pages/Manager/GalleryDetail.tsx
+++ frontend/artlink-front/src/pages/Manager/GalleryDetail.tsx
@@ -1,22 +1,80 @@
-// import React from 'react';
-import "./style/Detail.css";
-import ProfileBox from "../../commponents/Mypage/ProfileBox.tsx";
-import UserInputInfoTable from "../../commponents/Info/UserInputInfoTable.tsx";
-
-const id = 1;
-const name = "이름";
-const phone = "010-0000-0000";
+import { useState } from "react";
+import GDApi from "./GDApi";
+import { OneGalleryEach } from "../../api/ManagerApi";
+import Styles from "../../commponents/Mypage/Profile.module.css";
+import Styles2 from "../../pages/Common/Mypage.module.css";
+import BackBtn from "../../commponents/Base/BackBtn";
+import MarginTopInput from "../../commponents/EditCss/MaginTopInput";
 
 function GalleryDetail() {
+  const [galleryData, setgalleryData] = useState<OneGalleryEach | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // 자식 컴포넌트에서 받아온 데이터를 상태에 저장하는 콜백 함수
+  const handleGalleryInfoData = (data: OneGalleryEach) => {
+    console.log(data);
+    setgalleryData(data);
+    setLoading(false); // Data has been fetched, set loading to false
+  };
+
   return (
-    <div className="detail-container">
-      <ProfileBox></ProfileBox>
-      <UserInputInfoTable
-        id={id}
-        name={name}
-        phone={phone}
-      ></UserInputInfoTable>
-    </div>
+    <>
+      {/* 뒤로가기버튼 */}
+      <div className={Styles.BackBtn}>
+        <BackBtn />
+      </div>
+      {/* 프로필 컨테이너 */}
+      <div className={Styles2.Mypagecontainer}>
+        {loading ? ( // Show loading message if data is being fetched
+          <>
+            <h3>Loading...</h3>
+            <p>The server is under maintenance. Please try again later.</p>
+          </>
+        ) : (
+          <div className={Styles.MypageinnerBox}>
+            {/* 프로필 데이터 */}
+            {galleryData && (
+              <div className={Styles.infoOuterBoxRightGal}>
+                <div className={Styles.infoInnerBoxRightGal}>
+                  <p style={{ fontSize: "25px", fontWeight: "600" }}>
+                    Gallery Information
+                  </p>
+                  {Object.keys(galleryData).map((key) => (
+                    <div key={key}>
+                      {key === "description" ? ( // Check if the key is "description"
+                        <>
+                          <p>{key}:</p>
+                          <textarea
+                            name={key}
+                            value={galleryData[key]}
+                            className={Styles.profileTextArea}
+                          />
+                        </>
+                      ) : (
+                        <p key={key}>
+                          {key}:{" "}
+                          <input
+                            type="text"
+                            name={key}
+                            value={galleryData[key]}
+                            readOnly={key !== "description"}
+                            className={Styles.profileInput}
+                          />
+                        </p>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        <MarginTopInput value={40} />
+        {/* 갤러리 정보 로딩 컴포넌트 */}
+        <GDApi onGalleryDataChange={handleGalleryInfoData} />
+      </div>
+    </>
   );
 }
+
 export default GalleryDetail;

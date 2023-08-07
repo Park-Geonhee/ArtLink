8bcb7a44709165dc09404b9c9448d249584bc47a
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-07 23:49:27 +0900

    [FE] 새 컴포넌트 InfoBoard를 IoTBoard에 시범 적용

--- frontend/artlink-front/src/pages/Gallery/IoTBoard.tsx
+++ frontend/artlink-front/src/pages/Gallery/IoTBoard.tsx
@@ -1,14 +1,11 @@
 import styles from "./IoTBoard.module.css";
-import { Link } from "react-router-dom";
-import IoTTable from "../../commponents/Iot/IoTTable";
 import { useState, useEffect } from "react";
 import { Data, getDeviceList } from "../../api/IoTApi";
 import { setAuthorizationHeader } from "../../commponents/Base/BaseFun";
+import InfoBoard from "../../commponents/Info/InfoBoard";
 
 function IoTBoard() {
   const [deviceData, setDeviceData] = useState<Data[]>([]);
-  const [searchTerm, setSearchTerm] = useState(""); // 검색용
-  const [filteredData, setFilteredData] = useState<Data[]>([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -24,59 +21,20 @@ function IoTBoard() {
     void getData();
   });
 
-  // 검색어로 데이터 필터링
-  const filteringData = () => {
-    const filteredData = deviceData.filter((item) =>
-      Object.values(item).some((value) =>
-        value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-
-    setFilteredData(filteredData);
-  };
-
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      filteringData();
-    }
-  };
-
   const keys = ["Id", "Phone number"];
   const widths = ["20%", "80%"];
 
   return (
     <>
       <div className={styles.container}>
-        {/* 테이블 상단 */}
-        <div className={styles.wrapper}>
-          <Link to="/gallery/add-iot">
-            <button className={styles.addbutton}>+</button>
-          </Link>
-          <div>
-            <input
-              type="search"
-              placeholder="검색할 전화번호를 입력하세요."
-              className={styles.searchinput}
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-                filteringData();
-              }}
-              onKeyPress={handleKeyPress}
-            />
-            <button className={styles.searchbutton} onClick={filteringData}>
-              search
-            </button>
-          </div>
-        </div>
-        {/* 테이블 내용 */}
-        <div className={styles["table-wrapper"]}>
-          <IoTTable
-            data={searchTerm === "" ? deviceData : filteredData}
-            pageSize={10}
-            dataKeys={keys}
-            columnWidths={widths}
-          />
-        </div>
+        <InfoBoard
+          data={deviceData}
+          placeholder={"검색할 정보를 입력하세요"}
+          link={"/gallery/add-iot"}
+          dataKeys={keys}
+          columnWidths={widths}
+          detail={false}
+        />
       </div>
     </>
   );

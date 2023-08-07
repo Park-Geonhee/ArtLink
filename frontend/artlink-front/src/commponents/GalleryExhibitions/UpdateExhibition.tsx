015b2081c1e542dab310e5dd9cb897fde9fb6793
조준하 <whwnsgk830@naver.com>
2023-08-07 17:49:58 +0900

    [FE] Update App, WorksBoards Components & Add ExhibitionBoard, ExhibitionCreate Pages
    <App.tsx>
    - 갤러리 관련 url path 변경 (전시회 생성 단계로 인해)
    - 없는 url에 대해 404 Not Found path 연결
    
    <ExhibitionBoard>
    - 레이아웃 및 반응형 설계
    - 전체 전시회 조회 API 구현
    - Add 버튼으로 create 진입
    - Link로 각 전시회 Pk 이동
    - (중요) 이미지가 없어서 각 card 디자인 미적용
    
    <ExhibitionCreate>
    - 레이아웃 및 반응형 설계
    - 전시회 생성 API 구현
    - 성공시 모달창 띄우고 보드페이지로 이동
    - (중요) 이미지 API 미구현
    
    <WorksBoards>
    - 뒤로가기 버튼 전시회 이름 추가
    - 디자인 수정 (상단 메뉴바 반응형 수정)
    - 작품 목록 불러오기 API 구현
    
    <etc>
    - Artwork Manager 관련 url 연결 (메뉴, 빅버튼)
    - Add 버튼 추가 (Base 컴포넌트에 위치, 링크전달시 해당 링크로 이동하는 버튼)
    - Text 버튼 추가 ( 입력된 텍스트에 대한 버튼 크기조정 가능..? )
    - WorksBoard 의 link path 변경 (변경된 url로)

---
+++ frontend/artlink-front/src/commponents/GalleryExhibitions/UpdateExhibition.tsx
@@ -0,0 +1,10 @@
+import Styles from "./UpdateExhibition.module.css";
+
+function UpdateExhibition() {
+  return (
+    <>
+      <h1>UpdateExhibition</h1>
+    </>
+  );
+}
+export default UpdateExhibition;

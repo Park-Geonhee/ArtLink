a4e38aa67e0f63cef052a6627f88534fca1bdb46
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-08 16:05:33 +0900

    [FE] Code refactoring in LogIn, SignUp part

--- frontend/artlink-front/src/api/testSignup.tsx
+++ frontend/artlink-front/src/api/testSignup.tsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { SignupApi, SignupRes, SignupReq } from "./CommonApi";
+import { signUpUserApi, SignUpUserRes, SignUpReq } from "./CommonApi";
 // 해당 컴포넌트는 api통신 테스트입니다. get요청이고 post요청일시 datatosend변수로 매개변수를 전달하면 됩니다.
 
 const Apitest: React.FC = () => {
-  const [sampleData, setSampleData] = useState<SignupRes[]>([]);
-  const sendData: SignupReq = {
+  const [sampleData, setSampleData] = useState<SignUpUserRes>({
+    message: "",
+    data: {
+      user: {
+        username: "",
+        nickname: "",
+        phoneNumber: 0,
+      },
+    },
+  });
+  const sendData: SignUpReq = {
     username: "userrr",
     password: "1234",
     phoneNumber: 821022921491,
     nickname: "",
   };
+
   const getData = async () => {
     try {
-      const data = await SignupApi(sendData);
+      const data = await signUpUserApi(sendData);
       console.log(data);
       setSampleData(data);
     } catch (error) {
-      console.error("Error fetching SignupApi data:", error);
+      console.error("Error fetching signUpApi data:", error);
     }
   };
   useEffect(() => {
@@ -31,10 +41,10 @@ const Apitest: React.FC = () => {
       <hr />
       <div>
         {/* 가져온 데이터를 화면에 렌더링 */}
-        <p>{sampleData.id}</p>
-        <p>{sampleData.password}</p>
-        <p>{sampleData.role}</p>
-        <p>{sampleData.username}</p>
+        <p>{sampleData.message}</p>
+        <p>{sampleData.data.user.username}</p>
+        <p>{sampleData.data.user.nickname}</p>
+        <p>{sampleData.data.user.phoneNumber}</p>
       </div>
     </>
   );

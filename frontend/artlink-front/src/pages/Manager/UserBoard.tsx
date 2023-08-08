938c6abf467d1a58d511c7a07816c9b3e1433369
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-08 12:37:09 +0900

    [FE] Apply InfoBoard to UserBoard
    - UserBoard에 InfoBoard 적용

--- frontend/artlink-front/src/pages/Manager/UserBoard.tsx
+++ frontend/artlink-front/src/pages/Manager/UserBoard.tsx
@@ -2,7 +2,7 @@ import "./style/Board.css";
 import { useState, useEffect } from "react";
 import { UserGet, UserGetRes } from "../../api/ManagerApi";
 import { setAuthorizationHeader } from "../../commponents/Base/BaseFun";
-import InfoTable from "../../commponents/Info/InfoTable";
+import InfoBoard from "../../commponents/Info/InfoBoard";
 
 interface Data {
   [key: string]: string | number;
@@ -27,24 +27,21 @@ function UserBoard() {
 
   // 테이블 데이터
   const userData = AllUserData;
-  // const keys = Object.keys(userData[0]);
   const keys = ["PK", "아이디", "전화번호", "닉네임"]; // 데이터가 존재하지 않을 경우 오류가 발생하기 때문에 이 부분은 페이지 별로 하드코딩해야 함
   const widths = ["8%", "20%", "20%", "20%", "8%"];
-  const keyToExclude = [""];
 
   return (
     <>
-      <p className="board_title">User Manager</p>
-      <div className="board-container">
-        <div className="component-wrapper">
-          <InfoTable
-            data={userData}
-            pageSize={10}
-            dataKeys={keys}
-            columnWidths={widths}
-            keyToExclude={keyToExclude}
-          />
-        </div>
+      <div className="container">
+        <p className="board_title">User Manager</p>
+      </div>
+      <div className="container">
+        <InfoBoard
+          data={userData}
+          link={"/user-board/create"}
+          dataKeys={keys}
+          columnWidths={widths}
+        />
       </div>
     </>
   );

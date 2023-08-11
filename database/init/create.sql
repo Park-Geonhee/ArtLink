24b37dc11881d324756e2e41363e50813a9ba744
Ubuntu <ubuntu@ip-172-26-9-218.ap-northeast-2.compute.internal>
2023-08-11 06:54:04 +0000

    [BE] add docker file

--- database/init/create.sql
+++ database/init/create.sql
@@ -1,9 +1,9 @@
 USE mysql;
 FLUSH PRIVILEGES;
 create user 'admin1'@'%' identified by 's09_a202_mysql';
-create user 'admin2'@'%' identified by 's09_a202_mysql';
-GRANT ALL PRIVILEGES ON artlink.* TO 'admin1'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON bridge.* TO 'admin2'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON artlink.* TO 'admin1'@'%';
+GRANT ALL PRIVILEGES ON bridge.* TO 'admin1'@'%';
 CREATE DATABASE artlink;
 CREATE DATABASE bridge;
+CREATE DATABASE test;
 FLUSH PRIVILEGES;

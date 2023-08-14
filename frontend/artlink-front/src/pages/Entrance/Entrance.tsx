b6b16d1781a85b3582b1e6f64318afdc93c93a04
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-08-14 17:36:19 +0900

    [FE] Add animation in Entrance page
    - 개쩌는 애니메이션 추가
    - 추후 더 추가 예정

--- frontend/artlink-front/src/pages/Entrance/Entrance.tsx
+++ frontend/artlink-front/src/pages/Entrance/Entrance.tsx
@@ -1,24 +1,46 @@
-// import React from 'react';
+import { useState } from "react";
 import MainLogo from "../../commponents/Base/MainLogo";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Styles from "./Entrance.module.css";
 import MarginTopInput from "../../commponents/EditCss/MaginTopInput";
 
 function Entrance() {
+  const navigate = useNavigate();
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const handleStartButtonClick = () => {
+    setShowAnimation(true);
+
+    setTimeout(() => {
+      // 애니메이션 작동 코드
+      navigate("/login");
+    }, 1000); // 1초 동안 애니메이션 표시
+  };
+
   return (
-    <>
-      <div className={Styles.entranceContainer}>
-        <MainLogo />
-        <div className={Styles.startbtnBody}>
-          <Link to="/login" style={{ textDecoration: "none" }}>
-              <div className={Styles.startbtn}>
-                <p className={Styles.innerIcon}>{"start"}</p>
-              </div>
-          </Link>
-        </div>
-        <MarginTopInput value={100} />
+    <div
+      className={`${Styles.entranceContainer} ${
+        showAnimation ? Styles.animateContainer : ""
+      }`}
+    >
+      <MainLogo />
+      <div
+        className={`${Styles.startbtnBody} ${
+          showAnimation ? Styles.animateStartbtnBody : ""
+        }`}
+      >
+        <button
+          className={`${Styles.startbtn}  ${
+            showAnimation ? Styles.animateStartbtn : ""
+          }`}
+          onClick={handleStartButtonClick}
+        >
+          <p className={Styles.innerIcon}>{"start"}</p>
+        </button>
       </div>
-    </>
+      <MarginTopInput value={100} />
+    </div>
   );
 }
+
 export default Entrance;

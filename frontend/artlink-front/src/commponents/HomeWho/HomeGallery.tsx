f7379900a15dc75a3b7869e3433327aa1ec1e4b8
Baejw0111 <bjw0111@naver.com>
2023-08-16 00:44:32 +0900

    [FE] Add animations in login page, home page
    - 로그인 페이지, 홈 페이지에 애니메이션 추가
    - 홈 페이지 컴포넌트화로 코드 최적화

--- frontend/artlink-front/src/commponents/HomeWho/HomeGallery.tsx
+++ frontend/artlink-front/src/commponents/HomeWho/HomeGallery.tsx
@@ -1,10 +1,7 @@
-// import React from "react";
-import { Link } from "react-router-dom";
-import MainLogo from "../../commponents/Base/MainLogo";
-import Styles from "./../../commponents/Base/BigButton.module.css";
 import { GalleryInfo } from "../../api/GalleryApi";
 import { setAuthorizationHeader } from "../Base/BaseFun";
 import { useEffect, useState } from "react";
+import HomeBase from "./HomeBase";
 
 function HomeGallery() {
   const [accepted, setAccepted] = useState(true);
@@ -25,47 +22,15 @@ function HomeGallery() {
 
   return (
     <>
-      <MainLogo />
-      <div className="BigBtnBox2">
-        {/* Menu : Artwork Manage */}
-        <div className={Styles.startbtnBody}>
-          {accepted ? (
-            <Link
-              to="/exhibition-board"
-              style={{ textDecoration: "none" }}
-              className={Styles.startbtn}
-            >
-              <div style={{ margin: "auto" }}>
-                <p className={Styles.innerIcon}>{"Artwork Manager"}</p>
-                <p className={Styles.innerIcon2}>{"Register your work"}</p>
-              </div>
-            </Link>
-          ) : (
-            <div style={{ textDecoration: "none" }} className={Styles.startbtn}>
-              <div style={{ margin: "auto" }}>
-                <p className={Styles.innerIcon}>{"Artwork Manager"}</p>
-                <p className={Styles.innerIcon2}>{"Register your work"}</p>
-                <p style={{ color: "red" }} className={Styles.innerIcon}>
-                  {"Authorization required!"}
-                </p>
-              </div>
-            </div>
-          )}
-        </div>
-        {/* Menu : My page */}
-        <div className={Styles.startbtnBody}>
-          <Link
-            to="/mypage/gallery"
-            style={{ textDecoration: "none" }}
-            className={Styles.startbtn}
-          >
-            <div style={{ margin: "auto" }}>
-              <p className={Styles.innerIcon}>{"My Page"}</p>
-              <p className={Styles.innerIcon2}>{"Manage your gallery info"}</p>
-            </div>
-          </Link>
-        </div>
-      </div>
+      <HomeBase
+        firstLink="/exhibition-board"
+        firstTitle="Artwork Manager"
+        firstQuote="Register your work"
+        secondLink="/mypage/gallery"
+        secondTitle="My Page"
+        secondQuote="Manage your gallery info"
+        accepted={accepted}
+      />
     </>
   );
 }
--- frontend/artlink-front/src/commponents/HomeWho/HomeUser.tsx
+++ frontend/artlink-front/src/commponents/HomeWho/HomeUser.tsx
@@ -1,40 +1,16 @@
-// import React from "react";
-import { Link } from "react-router-dom";
-import MainLogo from "../../commponents/Base/MainLogo";
-import Styles from "./../../commponents/Base/BigButton.module.css";
+import HomeBase from "./HomeBase";
 
 function HomeUser() {
   return (
     <>
-      <MainLogo />
-      <div className="BigBtnBox2">
-        {/* Menu : Your Memory */}
-        <div className={Styles.startbtnBody}>
-          <Link
-            to="/art-memory"
-            style={{ textDecoration: "none" }}
-            className={Styles.startbtn}
-          >
-            <div style={{ margin: "auto" }}>
-              <p className={Styles.innerIcon}>{"Your Memory"}</p>
-              <p className={Styles.innerIcon2}>{"Exhibition Journey"}</p>
-            </div>
-          </Link>
-        </div>
-        {/* Menu : My Page */}
-        <div className={Styles.startbtnBody}>
-          <Link
-            to="/mypage/user"
-            style={{ textDecoration: "none" }}
-            className={Styles.startbtn}
-          >
-            <div style={{ margin: "auto" }}>
-              <p className={Styles.innerIcon}>{"My Page"}</p>
-              <p className={Styles.innerIcon2}>{"Read and Change Your Info"}</p>
-            </div>
-          </Link>
-        </div>
-      </div>
+      <HomeBase
+        firstLink="/art-memory"
+        firstTitle="Your Memory"
+        firstQuote="Exhibition Journey"
+        secondLink="/mypage/user"
+        secondTitle="My Page"
+        secondQuote="Read and Change Your Info"
+      />
     </>
   );
 }

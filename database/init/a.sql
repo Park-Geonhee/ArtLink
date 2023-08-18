13f31851b3fb225b6a9915038da36bc36b3bd646
Ubuntu <ubuntu@ip-172-26-9-218.ap-northeast-2.compute.internal>
2023-08-18 00:47:25 +0000

    fix : database init
    Former-commit-id: 134909df0d8bb440ad0565c4ed9417dfc9ae0d97

--- database/init/a.sql
+++ database/init/a.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:database/init/create.sql
 USE mysql;
 FLUSH PRIVILEGES;
 CREATE DATABASE artlink;
@@ -7,14 +6,3 @@ create user 'admin1'@'%' identified by 's09_a202_mysql';
 GRANT ALL PRIVILEGES ON artlink.* TO 'admin1'@'%';
 GRANT ALL PRIVILEGES ON bridge.* TO 'admin1'@'%';
 FLUSH PRIVILEGES;
-=======
-USE mysql;
-FLUSH PRIVILEGES;
-CREATE DATABASE artlink;
-CREATE DATABASE bridge;
-CREATE DATABASE test;
-create user 'admin1'@'%' identified by 's09_a202_mysql';
-GRANT ALL PRIVILEGES ON artlink.* TO 'admin1'@'%';
-GRANT ALL PRIVILEGES ON bridge.* TO 'admin1'@'%';
-FLUSH PRIVILEGES;
->>>>>>> f7b655d7c4f410f5aaddd059e62661be77521c0e:database/init/a.sql

6bdd049c0dd7eaa4336c8d67363c52b00ec74a47
조준하 <whwnsgk830@naver.com>
2023-08-18 10:56:57 +0900

    [FE] Update many things
    Former-commit-id: 6bbbc460fb65f7221d62589546378fbad1cce125 [formerly 35a4bbee3b9f778899edb42fbaae6ae49a335f5b]
    Former-commit-id: 8a8e42113b6fc68bbfb7a92f03ce880c524dbf69

--- frontend/artlink-front/src/pages/Other/AboutPage2.tsx
+++ frontend/artlink-front/src/pages/Other/AboutPage2.tsx
@@ -1,22 +1,22 @@
 import "./AboutUs.css";
-import aboutImg1 from "../../assets/About2-1.png";
-import aboutImg2 from "../../assets/About2-2.png";
-import aboutImg3 from "../../assets/About2-3.png";
-import aboutImg4 from "../../assets/About2-4.png";
-import aboutImg5 from "../../assets/About2-5.png";
-import aboutImg6 from "../../assets/About2-6.png";
+import aboutImg1 from "../../assets/About2-11.png";
+import aboutImg2 from "../../assets/About2-22.png";
+import aboutImg3 from "../../assets/About2-33.png";
+import aboutImg4 from "../../assets/About2-44.png";
+import aboutImg5 from "../../assets/About2-55.png";
+import aboutImg6 from "../../assets/About2-66.png";
 
 function AboutPage2() {
   return (
     <>
       <div className="imgOuter">
         <div className="horizontalscrollcontainer">
-        <img src={aboutImg1} alt="" />
-        <img src={aboutImg2} alt="" />
-        <img src={aboutImg3} alt="" />
-        <img src={aboutImg4} alt="" />
-        <img src={aboutImg5} alt="" />
-        <img src={aboutImg6} alt="" />
+          <img src={aboutImg1} alt="" />
+          <img src={aboutImg2} alt="" />
+          <img src={aboutImg3} alt="" />
+          <img src={aboutImg4} alt="" />
+          <img src={aboutImg5} alt="" />
+          <img src={aboutImg6} alt="" />
         </div>
       </div>
     </>
